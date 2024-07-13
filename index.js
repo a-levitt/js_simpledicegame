23ab2724f4542e3ef45d7ba081f41bfe2939e8c2
Artemis Levitt <artemis.levitt@gmail.com>
2024-07-13 13:16:01 +0200

    fixed inner links

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let randomNumber1 = Math.floor((Math.random() * 6)) + 1;
-let randomDice1 = "/./images/dice" + randomNumber1 + ".png";
+let randomDice1 = "images/dice" + randomNumber1 + ".png";
 
 let randomNumber2 = Math.floor((Math.random() * 6)) + 1;
-let randomDice2 = "/./images/dice" + randomNumber2 + ".png";
+let randomDice2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".img1").setAttribute("src", randomDice1);
 document.querySelector(".img2").setAttribute("src", randomDice2);
